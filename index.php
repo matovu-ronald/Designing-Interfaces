0f99445f066c3d0960a996d71fb859a66bb3a14c
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-03-21 16:11:49 +0300

    Refactor posts display

--- index.php
+++ index.php
@@ -1,11 +1,36 @@
-<?php 
+<?php
 
 require_once "src/config.php";
 
-$content = new Collection();
+if (isset($_GET['id'])) {
+    $content = new Posts(
+        $repo,
+        filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_SANITIZE_NUMBER_INT
+        )
+    );
+}
 
-$title = "Matovu Ronald Blog";
+if (isset($content) && $content->count() == 1 && $content->current()->status == "published") {
+    $title = $content->current()->title;
+} else {
+    $content = new Posts($repo);
+    $title = "Maron Blog";
+}
 
 require 'views/header.php';
+
+// var_dump($repo->find('posts', 1));
+// var_dump($repo->all('posts'));
+if ($content->count() == 1) {
+    include 'views/single.php';
+} else {
+    foreach ($content as $item) {
+        // echo $item->title;
+        include 'views/list.php';
+    }
+}
 
 require 'views/footer.php';
