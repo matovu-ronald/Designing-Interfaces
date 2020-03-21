566a1f24639daaa7797cbf7e4cae565d1cdc016d
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-03-21 16:10:35 +0300

    track view

--- views/track.php
+++ views/track.php
@@ -1,6 +1,6 @@
 <div class="group1 col-sm-6 col-md-6">
     <span class="fa fa-user"></span>
-    <?php //author ?>
+    <?php $content->getAuthor(); ?>
     <span class="fa fa-clock-o"></span>
-    <?php //date ?>
+    <?php echo $content->getDateTime(); ?>
 </div>
