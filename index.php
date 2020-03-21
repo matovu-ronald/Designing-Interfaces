061362bd54c7c65cf3d71d5005b132a7cb9f1710
Matovu Ronald <ronald.matovu@tollandhaben.com>
2020-03-21 17:39:49 +0300

    create new index.php file

--- index.php
+++ index.php
@@ -2,35 +2,17 @@
 
 require_once "src/config.php";
 
-if (isset($_GET['id'])) {
-    $content = new Posts(
-        $repo,
-        filter_input(
-            INPUT_GET,
-            'id',
-            FILTER_SANITIZE_NUMBER_INT
-        )
-    );
+$slug = 'home'; 
+
+
+if (isset($_GET['slug'])) {
+    $slug = filter_input(INPUT_GET, 'slug', FILTER_SANITIZE_STRING); 
 }
 
-if (isset($content) && $content->count() == 1 && $content->current()->status == "published") {
-    $title = $content->current()->title;
-} else {
-    $content = new Posts($repo);
-    $title = "Maron Blog";
-}
+$content = new Pages($repo, $slug, 'slug');
 
 require 'views/header.php';
 
-// var_dump($repo->find('posts', 1));
-// var_dump($repo->all('posts'));
-if ($content->count() == 1) {
-    include 'views/single.php';
-} else {
-    foreach ($content as $item) {
-        // echo $item->title;
-        include 'views/list.php';
-    }
-}
+require 'views/single.php';
 
 require 'views/footer.php';
